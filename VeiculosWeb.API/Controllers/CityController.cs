8f21aef6eb7d0877e851fe1d50e86107ccd4971a
Gustavo Silva <gustavohs2004@gmail.com>
2024-08-02 11:21:35 -0300

    Criando controllers

---
+++ VeiculosWeb.API/Controllers/CityController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using VeiculosWeb.Domain.Enum;
+using VeiculosWeb.Infrastructure.Service;
+using VeiculosWeb.Utils;
+
+namespace VeiculosWeb.API.Controllers
+{
+    public class CityController(ICityService cityService) : BaseController
+    {
+        [HttpPost("Sync")]
+        [Authorize(Roles = nameof(RoleName.Admin))]
+        public async Task<IActionResult> Sync()
+        {
+            var city = await cityService.SyncCities();
+            return StatusCode(city.Code, city);
+        }
+        
+        [HttpGet("GetCitiesByState/{stateId:Guid}")]
+        [OutputCache(PolicyName = "CacheImmutableResponse", Duration = Consts.CacheTimeout)]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetCitiesByState([FromRoute]Guid stateId)
+        {
+            var city = await cityService.GetCitiesByState(stateId);
+            return StatusCode(city.Code, city);
+        }
+    }
+}
---
+++ VeiculosWeb.API/Controllers/StateController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using VeiculosWeb.Domain.Enum;
+using VeiculosWeb.Infrastructure.Service;
+using VeiculosWeb.Utils;
+
+namespace VeiculosWeb.API.Controllers
+{
+    public class StateController(IStateService stateService) : BaseController
+    {
+        [HttpPost("Sync")]
+        [Authorize(Roles = nameof(RoleName.Admin))]
+        public async Task<IActionResult> Sync()
+        {
+            var state = await stateService.SyncStates();
+            return StatusCode(state.Code, state);
+        }
+        
+        [HttpGet("GetStates")]
+        [OutputCache(PolicyName = "CacheImmutableResponse", Duration = Consts.CacheTimeout)]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetStates()
+        {
+            var state = await stateService.GetStates();
+            return StatusCode(state.Code, state);
+        }
+    }
+}
